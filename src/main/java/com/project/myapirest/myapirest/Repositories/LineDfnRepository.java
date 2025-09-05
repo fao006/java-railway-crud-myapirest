5c5327bc6ac446c44ea3ad97b75a951928055c00
fao006 <600oaf@gmail.com>
2025-09-05 14:44:28 -0600

    Delete src directory

--- src/main/java/com/project/myapirest/myapirest/Repositories/LineDfnRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.myapirest.myapirest.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.myapirest.myapirest.Entities.LineDfn;
-
-public interface LineDfnRepository extends JpaRepository<LineDfn, Long>{
-
-}
--- src/main/java/com/project/myapirest/myapirest/Repositories/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.myapirest.myapirest.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.myapirest.myapirest.Entities.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long>{
-
-}
